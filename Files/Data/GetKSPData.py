c1c76aa27cc96bb7c2a2ebdbcbfdf0362c8cc7fa
Alex <borrussiagame@mail.ru>
2024-12-05 23:04:20 +0300

    fix

--- Files/Data/GetKSPData.py
+++ Files/Data/GetKSPData.py
@@ -13,7 +13,10 @@ earth = conn.space_center.bodies["Kerbin"]
 ss = "время, масса, высота, скорость"
 t = 0
 while vessel.orbit.periapsis_altitude < 200000:
-    ss += f"\n{t:.2f}, {vessel.mass:.2f}, {vessel.flight().mean_altitude:.2f}, {numpy.linalg.norm(vessel.velocity(earth.reference_frame)):.2f}"
+    if vessel.flight().mean_altitude < 35000:
+        ss += f"\n{t:.2f}, {vessel.mass:.2f}, {vessel.flight().mean_altitude:.2f}, {numpy.linalg.norm(vessel.velocity(earth.reference_frame)):.2f}"
+    else:
+        ss += f"\n{t:.2f}, {vessel.mass:.2f}, {vessel.flight().mean_altitude:.2f}, {vessel.orbit.speed:.2f}"
     t += 0.02
     time.sleep(0.02)
 
